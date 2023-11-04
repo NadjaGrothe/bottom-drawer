94375bcc35d5d550c865ea0917fa38150bcd4999
Nadja Grothe <nadjagrothe@gmail.com>
2023-11-04 10:42:10 +0000

    created useGesture hook with swipe support

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import './App.css';
 
 import BottomSheet from './components/BottomSheet';
 
+/* //TODO:
+- Create a Card component to display inside the BottomSheet
+  - 50px height and any background color
+- Create an Array of 100 and map over it to render 100 Cards
+- Assure that the BottomSheet is scrollable
+- write tests
+- refactor
+- Deploy & share repo
+
+Bonus:
+- Add additional styling (App background, Card, BottomSheet, animations)
+- Write Documentation
+*/
+
 function App() {
   return (
     <>
       <h1>Bottom Sheet</h1>
-      <BottomSheet title='1900+ homes available in Orlando'>
+      <BottomSheet title="1900+ homes available in Orlando">
         <p>Bottom Sheet Content</p>
       </BottomSheet>
     </>
--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,5 +1,18 @@
+/* Functionality:
+
+  - swipe events will trigger the snapPoints to move to the next one in the direction of the swipe (i.e. closed -> half -> full)
+  - drag events 
+    - allow the user to move the sheet to a specific snapPoint, based on the position of the sheet when the drag event ends
+    - sheet should snap to the closest snapPoint or based on defined thresholds (i.e. if the sheet is dragged in the bottom 25% of the screen, it should snap to the closed position, above 60% it should snap to the full position, and between 25% and 60% it should snap to the half position) // TODO: decide on functionality - HSBC app seems to have defined thresholds
+    - sheet will have to move with the user's finger
+  - tap events // TODO: decide on functionality - Could go between closed and half only, or closed and full, or cycle through all three positions
+
+*/
+
+import { useEffect, useState } from 'react';
+
 import styles from './BottomSheet.module.scss';
-import { useState } from 'react';
+import useGesture from '../hooks/useGesture';
 
 type TSnapPoints = 'closed' | 'half' | 'full';
 interface IBottomSheet {
@@ -7,33 +20,43 @@ interface IBottomSheet {
   title: string;
 }
 
+/* TODO:
+
+  - [ ] add support for tap events
+  - [ ] add support for drag events
+  - [ ] refactor
+
+*/
+
 const BottomSheet = ({ children, title }: IBottomSheet) => {
   const [sheetPosition, setSheetPosition] = useState<TSnapPoints>('closed');
+  const { handleTouchStart, handleTouchMove, handleTouchEnd, direction } = useGesture();
+
+  useEffect(() => {
+    if (direction === 'up') {
+      if (sheetPosition === 'closed') {
+        setSheetPosition('half');
+      } else if (sheetPosition === 'half') {
+        setSheetPosition('full');
+      }
+    } else if (direction === 'down') {
+      if (sheetPosition === 'full') {
+        setSheetPosition('half');
+      } else setSheetPosition('closed');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [direction]);
 
   return (
-    <div className={`${styles.bottomSheet} ${styles[sheetPosition]}`}>
-      <div
-        className={styles.header}
-        onClick={() => {
-          // TODO remove this
-          /*
-          Create function/hook that:
-          - will adjust sheetPosition based on user interaction
-            - HSBC banking app has good example of this
-            - depending on how user drags sheet, it will snap to different positions
-            - tapping/clicking on header will open sheet to half
-          */
-          if (sheetPosition === 'closed') {
-            setSheetPosition('half');
-          } else if (sheetPosition === 'half') {
-            setSheetPosition('full');
-          } else {
-            setSheetPosition('closed');
-          }
-        }}
-      >
+    <div
+      className={`${styles.bottomSheet} ${styles[sheetPosition]}`}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
+      <div className={styles.header}>
         <div className={styles.handle} />
-        <h5 className="bottom-sheet__title">{title}</h5>
+        <h2>{title}</h2>
       </div>
       {sheetPosition !== 'closed' && <div className="bottom-sheet__content">{children}</div>}
     </div>
